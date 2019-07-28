ae1f29abba21758596ccc41d7a93334ed200e315
Roman Arrow <romanarrow.fca@gmail.com>
2019-07-28 22:17:03 +0800

    ripcurlgo

---
+++ precache-manifest.89e6af710228d7feb1551aba75739b7b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3675cf7c17ecf88078670e9cf5e34b02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "722eeeecb0859d5db21a",
+    "url": "/static/css/2.60cf962c.chunk.css"
+  },
+  {
+    "revision": "035359c0cbd69ff7562d",
+    "url": "/static/css/main.25a103c3.chunk.css"
+  },
+  {
+    "revision": "722eeeecb0859d5db21a",
+    "url": "/static/js/2.36845707.chunk.js"
+  },
+  {
+    "revision": "035359c0cbd69ff7562d",
+    "url": "/static/js/main.6d816e02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a94a58d2e0ab5dada433f9653e47e3eb",
+    "url": "/static/media/beach-surfer.a94a58d2.jpg"
+  },
+  {
+    "revision": "1aa5f20020f4335884dc822820b6ad59",
+    "url": "/static/media/bg-login.1aa5f200.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7588bec8bf6e3bf12c5bebafb28982e1",
+    "url": "/static/media/little-surfer.7588bec8.jpg"
+  },
+  {
+    "revision": "917b1ba33f041b364a41bd24b0d4affd",
+    "url": "/static/media/logo.917b1ba3.png"
+  },
+  {
+    "revision": "065449a7128e744bbdcaab2b8ebd0823",
+    "url": "/static/media/power-surfer.065449a7.jpg"
+  },
+  {
+    "revision": "209b6fda668605b3193ecc24476e89e6",
+    "url": "/static/media/reef-surfer.209b6fda.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "1a3c45e348acd391fb3f23567b414a5a",
+    "url": "/static/media/tide.1a3c45e3.png"
+  }
+]);
